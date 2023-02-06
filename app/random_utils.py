27c44eed38ca943c8ca04007af8981412c75e1f3
dsymbol <dsymbol@users.noreply.github.com>
2023-02-06 23:00:10 +0200

    embed powershell script as a multiline string

--- app/random_utils.py
+++ app/random_utils.py
@@ -34,8 +34,36 @@ def random_mac_address():
     """
     :return: set random mac address
     """
-    script_path = os.path.join(os.path.dirname(__file__), "mac_change.ps1")
-    command = ['powershell', '-Exec', 'Bypass', f'& {script_path}']
+    script = """
+    $interfaces = Get-NetAdapter -Name * -Physical
+    $ULIGBits = 0, 4, 8, "C"
+
+    function genRandMAC {
+    	for($i = 0; $i -lt 12; $i++){
+    		if($i -eq 1){
+    			$MAC += "{0:X}" -f (Get-Random $ULIGBits)
+    		}
+    		else{
+    			$MAC += "{0:X}" -f (Get-Random -min 0 -max 16)
+    		}
+    	}
+    	return $MAC
+    }
+
+    foreach ($interface in $interfaces)
+    {
+        do{
+            $randMAC = genRandMAC
+            Set-NetAdapter -Name $interface.Name -MacAddress $randMAC -Confirm:0
+        } while($randMAC -eq $interface.MacAddress)
+
+        if ($interface.MacAddress -eq (Get-NetAdapter -Name $interface.Name).MacAddress){
+            throw
+        }
+        Write-Host -NoNewline (Get-NetAdapter -Name $interface.Name).MacAddress
+    }
+    """
+    command = ['powershell', '-Exec', 'Bypass', script]
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     message = "UNCHANGED ? ERROR" if result.returncode != 0 else result.stdout
     return message
